6e17afd7c3dca76bf6ef48d9ca6da1974056affd
Krystian <krystian1wojtowicz@gmail.com>
2022-06-25 10:06:08 +0200

    3

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ const addBookBtn = document.querySelector(".add-book");
 const submit = document.querySelector(".submit");
 const books = document.querySelector(".wrapper-grid");
 
-let myLibrary = [];
+let myLibrary = [{
+  title: 'hobbit',
+  author: 'me',
+  pages: '2'
+},
+{
+  title: 'hobbit2',
+  author: 'you',
+  pages: '3'
+}];
 
 addBookBtn.addEventListener("click", () => {
   form.style.display = "block";
@@ -38,3 +47,13 @@ const addBookToLibrary = (e) => {
 document.addEventListener("DOMContentLoaded", () => {
   submit.addEventListener("click", addBookToLibrary);
 });
+
+const display = myLibrary.forEach(book => {
+  const div = document.createElement("div");
+  div.classList.add("container");
+  books.appendChild(div);
+  const divtitle = document.createElement("div");
+  div.appendChild(divtitle);
+  divtitle.textContent = book.title;
+});
+
