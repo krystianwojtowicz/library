91e09b8a5d804ecc563ab17c361f46ae6cb29d20
Krystian <krystian1wojtowicz@gmail.com>
2022-06-25 08:58:13 +0200

    2.5

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 const form = document.querySelector("form");
 const addBookBtn = document.querySelector(".add-book");
 const submit = document.querySelector(".submit");
+const books = document.querySelector(".wrapper-grid");
 
 let myLibrary = [];
 
@@ -18,16 +19,22 @@ const addBookToLibrary = (e) => {
   e.preventDefault();
   let book = {
     id: Date.now(),
-    title: document.getElementById('title').value,
-    pages: document.getElementById('pages').value,
+    title: document.getElementById("title").value,
+    pages: document.getElementById("pages").value,
   };
   myLibrary.push(book);
   document.forms[0].reset();
-  console.warn({myLibrary});
-  let pre = document.querySelector('#msg pre');
-  pre.textContent = '\n' + JSON.stringify(myLibrary, '\t', 2);
-}
+  const div = document.createElement("div");
+  div.classList.add("container");
+  books.appendChild(div);
+  const divtitle = document.createElement("div");
+  div.appendChild(divtitle);
+  divtitle.textContent = book.title; 
+  console.warn({ myLibrary });
+  let pre = document.querySelector("#msg pre");
+  pre.textContent = "\n" + JSON.stringify(myLibrary, "\t", 2);
+};
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    submit.addEventListener('click', addBookToLibrary);
+document.addEventListener("DOMContentLoaded", () => {
+  submit.addEventListener("click", addBookToLibrary);
 });
